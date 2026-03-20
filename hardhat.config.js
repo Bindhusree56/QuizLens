67161ebde7037aa6482fb9679799fd3569ea70b9
Bindhusree56 <bindhusree564@gmail.com>
2026-03-20 13:09:21 +0530

    connecting ganache and changing ports

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ module.exports = {
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
-      chainId: 31337
+      chainId: 1337
     },
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
